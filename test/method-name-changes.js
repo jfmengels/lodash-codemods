3f08cf958af236a9c34d562a20d67fa2483c1fb2
Jeroen Engels <jfm.engels@gmail.com>
2016-05-13 15:42:09 +0200

    Handle renaming methods that are not called

--- test/method-name-changes.js
+++ test/method-name-changes.js
@@ -7,8 +7,12 @@ const with_ = (input) => `var _ = require('lodash');\n${input}`;
 const testUnchanged = (input) => test(input, input);
 
 test(with_('_.pluck(foo, bar)'), with_('_.map(foo, bar)'));
+
 test(with_('_.findWhere(foo, bar)'), with_('_.find(foo, bar)'));
+
 test(with_('_.where(foo, bar)'), with_('_.filter(foo, bar)'));
+
+test(with_('_.partial(_.pluck, bar)'), with_('_.partial(_.map, bar)'));
 
 testUnchanged(with_('_.map(foo, bar)'));
 testUnchanged(with_('pluck(foo, bar)'));
