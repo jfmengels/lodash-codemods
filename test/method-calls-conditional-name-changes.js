1eb5372d092a0f8d4b722241bba571442f3b2051
Jeroen Engels <jfm.engels@gmail.com>
2016-05-13 19:17:30 +0200

    Support migration of `merge`, `omit` and `sample`

--- test/method-calls-conditional-name-changes.js
+++ test/method-calls-conditional-name-changes.js
@@ -9,6 +9,12 @@ test(with_('_.assign(foo, bar, () => {})'), with_('_.assignWith(foo, bar, () => 
 testUnchanged(with_('_.assign(foo, bar)'));
 testUnchanged(with_('_.assign(foo, bar, baz)'));
 
+test(with_('_.merge(foo, bar, function() {})'), with_('_.mergeWith(foo, bar, function() {})'));
+test(with_('_.merge(foo, bar, function() {}, this)'), with_('_.mergeWith(foo, bar, function() {}, this)'));
+test(with_('_.merge(foo, bar, () => {})'), with_('_.mergeWith(foo, bar, () => {})'));
+testUnchanged(with_('_.merge(foo, bar)'));
+testUnchanged(with_('_.merge(foo, bar, baz)'));
+
 test(with_('_.clone(foo, false)'), with_('_.clone(foo)'));
 test(with_('_.clone(foo, true)'), with_('_.cloneDeep(foo)'));
 test(with_('_.clone(foo, false, customizer)'), with_('_.cloneWith(foo, customizer)'));
@@ -29,3 +35,11 @@ testUnchanged(with_('_.max(foo)'));
 
 test(with_('_.min(foo, iteratee)'), with_('_.minBy(foo, iteratee)'));
 testUnchanged(with_('_.min(foo)'));
+
+test(with_('_.omit(foo, () => {})'), with_('_.omitBy(foo, () => {})'));
+test(with_('_.omit(foo, () => {}, this)'), with_('_.omitBy(foo, () => {}, this)'));
+testUnchanged(with_('_.omitBy(foo, "a")'));
+testUnchanged(with_('_.omitBy(foo, ["a", "b"])'));
+
+test(with_('_.sample(foo, n)'), with_('_.sampleSize(foo, n)'));
+testUnchanged(with_('_.sample(foo)'));
