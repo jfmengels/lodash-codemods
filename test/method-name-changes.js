e45a53f2c2286cf9675ab57075a048ce693ea338
Jeroen Engels <jfm.engels@gmail.com>
2016-05-19 14:35:37 +0200

    Simplify `method-name-changes`

--- test/method-name-changes.js
+++ test/method-name-changes.js
@@ -3,30 +3,6 @@ import {testPlugin, with_} from './helpers/jscodeshift-wrapper';
 
 const {test, testUnchanged} = testPlugin(plugin);
 
-test(with_('_.pluck(foo, bar)'), with_('_.map(foo, bar)'));
-
-test(with_('_.findWhere(foo, bar)'), with_('_.find(foo, bar)'));
-
-test(with_('_.where(foo, bar)'), with_('_.filter(foo, bar)'));
-
-test(with_('_.partial(_.pluck, bar)'), with_('_.partial(_.map, bar)'));
-
-test(with_('_.first(foo, bar)'), with_('_.head(foo, bar)'));
-test(with_('_.indexBy(foo, bar)'), with_('_.keyBy(foo, bar)'));
-test(with_('_.invoke(foo, bar)'), with_('_.invokeMap(foo, bar)'));
-test(with_('_.modArgs(foo, bar)'), with_('_.overArgs(foo, bar)'));
-test(with_('_.padLeft(foo, bar)'), with_('_.padStart(foo, bar)'));
-test(with_('_.padRight(foo, bar)'), with_('_.padEnd(foo, bar)'));
-test(with_('_.pairs(foo, bar)'), with_('_.toPairs(foo, bar)'));
-test(with_('_.rest(foo, bar)'), with_('_.tail(foo, bar)'));
-test(with_('_.restParam(foo, bar)'), with_('_.rest(foo, bar)'));
-test(with_('_.sortByAll(foo, bar)'), with_('_.sortBy(foo, bar)'));
-test(with_('_.sortByOrder(foo, bar)'), with_('_.orderBy(foo, bar)'));
-test(with_('_.trimLeft(foo, bar)'), with_('_.trimStart(foo, bar)'));
-test(with_('_.trimRight(foo, bar)'), with_('_.trimEnd(foo, bar)'));
-test(with_('_.trunc(foo, bar)'), with_('_.truncate(foo, bar)'));
-test(with_('_.compose(foo, bar)'), with_('_.flowRight(foo, bar)'));
-
 test(with_('_.all(foo, bar)'), with_('_.every(foo, bar)'));
 test(with_('_.any(foo, bar)'), with_('_.some(foo, bar)'));
 test(with_('_.backflow(foo, bar)'), with_('_.flowRight(foo, bar)'));
@@ -35,14 +11,32 @@ test(with_('_.collect(foo, bar)'), with_('_.map(foo, bar)'));
 test(with_('_.compose(foo, bar)'), with_('_.flowRight(foo, bar)'));
 test(with_('_.contains(foo, bar)'), with_('_.includes(foo, bar)'));
 test(with_('_.detect(foo, bar)'), with_('_.find(foo, bar)'));
+test(with_('_.findWhere(foo, bar)'), with_('_.find(foo, bar)'));
+test(with_('_.first(foo, bar)'), with_('_.head(foo, bar)'));
 test(with_('_.foldl(foo, bar)'), with_('_.reduce(foo, bar)'));
 test(with_('_.foldr(foo, bar)'), with_('_.reduceRight(foo, bar)'));
 test(with_('_.include(foo, bar)'), with_('_.includes(foo, bar)'));
+test(with_('_.indexBy(foo, bar)'), with_('_.keyBy(foo, bar)'));
 test(with_('_.inject(foo, bar)'), with_('_.reduce(foo, bar)'));
+test(with_('_.invoke(foo, bar)'), with_('_.invokeMap(foo, bar)'));
 test(with_('_.methods(foo, bar)'), with_('_.functions(foo, bar)'));
+test(with_('_.modArgs(foo, bar)'), with_('_.overArgs(foo, bar)'));
 test(with_('_.object(foo, bar)'), with_('_.fromPairs(foo, bar)'));
+test(with_('_.padLeft(foo, bar)'), with_('_.padStart(foo, bar)'));
+test(with_('_.padRight(foo, bar)'), with_('_.padEnd(foo, bar)'));
+test(with_('_.pairs(foo, bar)'), with_('_.toPairs(foo, bar)'));
+test(with_('_.partial(_.pluck, bar)'), with_('_.partial(_.map, bar)'));
+test(with_('_.pluck(foo, bar)'), with_('_.map(foo, bar)'));
+test(with_('_.rest(foo, bar)'), with_('_.tail(foo, bar)'));
+test(with_('_.restParam(foo, bar)'), with_('_.rest(foo, bar)'));
 test(with_('_.select(foo, bar)'), with_('_.filter(foo, bar)'));
+test(with_('_.sortByAll(foo, bar)'), with_('_.sortBy(foo, bar)'));
+test(with_('_.sortByOrder(foo, bar)'), with_('_.orderBy(foo, bar)'));
+test(with_('_.trimLeft(foo, bar)'), with_('_.trimStart(foo, bar)'));
+test(with_('_.trimRight(foo, bar)'), with_('_.trimEnd(foo, bar)'));
+test(with_('_.trunc(foo, bar)'), with_('_.truncate(foo, bar)'));
 test(with_('_.unique(foo, bar)'), with_('_.uniq(foo, bar)'));
+test(with_('_.where(foo, bar)'), with_('_.filter(foo, bar)'));
 
 testUnchanged(with_('_.map(foo, bar)'));
 testUnchanged(with_('pluck(foo, bar)'));
