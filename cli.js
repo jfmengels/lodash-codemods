7c141646ece5cc13acbe12236a7556084e30b9d0
Jeroen Engels <jfm.engels@gmail.com>
2016-05-19 16:18:09 +0200

    Add CLI using `lib-upgrader`

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict';
+
+var upgrader = require('lib-upgrader');
+var pkg = require('./package.json');
+var releases = require('./releases.json');
+
+var settings = {
+  libraryName: 'Lodash',
+  releases: releases,
+  pkg: pkg,
+  dirname: __dirname
+};
+
+upgrader.handleCliArgs(settings)
+  .then(upgrader.checkForUpdates)
+  .then(upgrader.checkGitIsClean)
+  .then(upgrader.prompt)
+  .then(upgrader.applyCodemods)
+  .then(upgrader.printTip)
+  .catch(function (err) {
+    console.error(err.message);
+    process.exit(1);
+  });
